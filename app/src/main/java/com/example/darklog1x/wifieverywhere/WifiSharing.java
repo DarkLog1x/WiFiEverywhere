09b2b2f7b79329a2b597c2ee346e2a3fc35bc025
Aleksander Okonski <aleksander.oko@gmail.com>
2016-08-25 20:46:09 +0200

    Displaying wifi's to users

--- app/src/main/java/com/example/darklog1x/wifieverywhere/WifiSharing.java
+++ app/src/main/java/com/example/darklog1x/wifieverywhere/WifiSharing.java
@@ -5,17 +5,32 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.PixelFormat;
 import android.net.wifi.ScanResult;
+import android.net.wifi.WifiConfiguration;
 import android.net.wifi.WifiManager;
 import android.os.IBinder;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.view.WindowManager;
 import android.widget.Toast;
 
+import java.io.IOException;
 import java.util.List;
 
 public class WifiSharing extends Service {
     public WifiSharing() {
     }
+
+    List<ScanResult> mScanResults;
+    List<WifiConfiguration> KnownNetworks;
+    private View mView;
+
+    private WindowManager.LayoutParams mParams;
+    private WindowManager mWindowManager;
 
     private static final String TAG = "WiFi_Sharing";
     WifiManager mWifiManager;
@@ -37,10 +52,13 @@ public class WifiSharing extends Service {
         @Override
         public void onReceive(Context c, Intent intent) {
             if (intent.getAction().equals(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION)) {
-                List<ScanResult> mScanResults = mWifiManager.getScanResults();
+                mScanResults = mWifiManager.getScanResults();
+
                 for (int i = 0; i < mScanResults.size(); i++) {
                     Log.w("Wifi is", mScanResults.get(i).toString());
                 }
+                //Savednetworks();
+                displayNetworks();
             }
         }
 
@@ -57,7 +75,7 @@ public class WifiSharing extends Service {
         new Thread(new Runnable() {
             @Override
             public void run() {
-
+                onCreate();
             }
         }).start();
 
@@ -69,7 +87,123 @@ public class WifiSharing extends Service {
     public void onDestroy() {
         unregisterReceiver(mWifiScanReceiver);
         super.onDestroy();
+        ((WindowManager)getSystemService(WINDOW_SERVICE)).removeView(mView);
+        mView = null;
         Toast.makeText(this, "Service Destroyed", Toast.LENGTH_SHORT).show();
     }
 
+    private void Savednetworks() {
+        mWifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+        KnownNetworks = mWifiManager.getConfiguredNetworks();
+        //getPrivilegedConfiguredNetworks
+
+        for (int i = 0; i < KnownNetworks.size(); i++) {
+            Log.w("Wifi is", KnownNetworks.get(i).toString());
+        }
+        ///data/misc/wifi
+
+//        Runtime runtime = Runtime.getRuntime();
+//        try {
+//            Process mIpAddrProcess = runtime.exec("/system/bin/cat /date/misc/wifi/wpa_suppliciant.conf");
+//            int mExitValue = mIpAddrProcess.waitFor();
+//            System.out.println(" mExitValue " + mExitValue);
+//
+//        } catch (InterruptedException ignore) {
+//            ignore.printStackTrace();
+//            System.out.println(" Exception:" + ignore);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//            System.out.println(" Exception:" + e);
+//        }
+
+    }
+
+    private void connetctToAndSetWifFiNetwork(String SSID, String Password) {
+        WifiManager wifi = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+        WifiConfiguration wc = new WifiConfiguration();
+// This is must be quoted according to the documentation
+// http://developer.android.com/reference/android/net/wifi/WifiConfiguration.html#SSID
+        wc.SSID = '"' + SSID + '"';
+        wc.preSharedKey = '"' + Password + '"';
+        wc.hiddenSSID = false;
+        wc.status = WifiConfiguration.Status.ENABLED;
+        wc.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.TKIP);
+        wc.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.CCMP);
+        wc.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.WPA_PSK);
+        wc.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.TKIP);
+        wc.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.CCMP);
+        wc.allowedProtocols.set(WifiConfiguration.Protocol.RSN);
+        int res = wifi.addNetwork(wc);
+        Log.d("WifiPreference", "add Network returned " + res);
+        boolean b = wifi.enableNetwork(res, true);
+        Log.d("WifiPreference", "enableNetwork returned " + b);
+    }
+
+    private void connetctToAndSetWifFiNetwork(String SSID) {
+        WifiManager wifi = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+        WifiConfiguration wc = new WifiConfiguration();
+// This is must be quoted according to the documentation
+// http://developer.android.com/reference/android/net/wifi/WifiConfiguration.html#SSID
+        wc.SSID = '"' + SSID + '"';
+        wc.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.NONE);
+        wc.hiddenSSID = false;
+        wc.status = WifiConfiguration.Status.ENABLED;
+        int res = wifi.addNetwork(wc);
+        Log.d("WifiPreference", "add Network returned " + res);
+        boolean b = wifi.enableNetwork(res, true);
+        Log.d("WifiPreference", "enableNetwork returned " + b);
+    }
+
+
+    private void displayNetworks() {
+        mView = new MyLoadView(this);
+
+        mParams = new WindowManager.LayoutParams(
+                WindowManager.LayoutParams.MATCH_PARENT, 150, 10, 10,
+                WindowManager.LayoutParams.TYPE_SYSTEM_DIALOG, //TYPE_SYSTEM_OVERLAY,
+                        WindowManager.LayoutParams.FLAG_LOCAL_FOCUS_MODE,
+                PixelFormat.TRANSLUCENT);
+
+        mParams.gravity = Gravity.CENTER;
+        mParams.setTitle("Window test");
+
+
+        mWindowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
+        mWindowManager.addView(mView, mParams);
+    }
+
+    public class MyLoadView extends View {
+
+        private Paint mPaint;
+
+        public MyLoadView(Context context) {
+            super(context);
+            mPaint = new Paint();
+            mPaint.setTextSize(50);
+            mPaint.setARGB(200, 200, 200, 200);
+        }
+
+        @Override
+        protected void onDraw(Canvas canvas) {
+            super.onDraw(canvas);
+            for (int i = 0; i < mScanResults.size(); i++) {
+                canvas.drawText(mScanResults.get(i).toString(), 0, 100, mPaint);
+            }
+        }
+
+        @Override
+        protected void onAttachedToWindow() {
+            super.onAttachedToWindow();
+        }
+
+        @Override
+        protected void onDetachedFromWindow() {
+            super.onDetachedFromWindow();
+        }
+
+        @Override
+        protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+            super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        }
+    }
 }
